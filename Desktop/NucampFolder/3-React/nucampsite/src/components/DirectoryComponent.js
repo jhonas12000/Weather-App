56946f4ccf03a8091035490cf4eb4180e7a6b792
Yonas <ytwmichael@gmail.com>
2020-10-28 13:23:41 -0700

    card form complete

--- Desktop/NucampFolder/3-React/nucampsite/src/components/DirectoryComponent.js
+++ Desktop/NucampFolder/3-React/nucampsite/src/components/DirectoryComponent.js
@@ -1,15 +1,19 @@
 import React  from 'react';
-import { Card, CardImg, CardImgOverlay, CardTitle } from 'reactstrap';
+import { Card, CardImg, CardImgOverlay, CardTitle, Breadcrumb, BreadcrumbItem } from 'reactstrap';
+import { Link } from 'react-router-dom';
+
 
     
-    function RenderDirectoryItem({campsite, onClick}) {
-        return (
-            <Card>
+function RenderDirectoryItem({campsite}) {
+    return (
+        <Card>
+            <Link to={`/directory/${campsite.id}`}>
                 <CardImg width="100%" src={campsite.image} alt={campsite.name} />
                 <CardImgOverlay>
                     <CardTitle>{campsite.name}</CardTitle>
                 </CardImgOverlay>
-            </Card>
+            </Link>
+        </Card>
         )
     }
     function Directory(props) {
@@ -23,9 +27,18 @@ import { Card, CardImg, CardImgOverlay, CardTitle } from 'reactstrap';
         return (
             <div className="container">
                 <div className="row">
+                    <div className="col">
+                        <Breadcrumb>
+                            <BreadcrumbItem><Link to="/home">Home</Link></BreadcrumbItem>
+                            <BreadcrumbItem active>Directory</BreadcrumbItem>
+                        </Breadcrumb>
+                        <h2>Directory</h2>
+                        <hr />
+                    </div>
+                </div>
+                <div className="row">
                     {directory}
                 </div>
-                
             </div>
         );
     }
